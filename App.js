eb08e07ed3ff1f04b02d436a090d5599fc77d6f9
Alejandro Gomez <alejandrogomez@alejandrosimac2.lan>
2019-11-18 18:13:21 -0500

    Created expo react-native app

---
+++ App.js
@@ -0,0 +1,33 @@
+import { AppLoading } from 'expo';
+
+import React, { useState } from 'react';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+import AppNavigator from './navigation/AppNavigator';
+
+export default function App(props) {
+  const [isLoadingComplete, setLoadingComplete] = useState(false);
+
+    return (
+      <View style={styles.container}>
+        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        <AppNavigator />
+      </View>
+    );
+  }
+
+function handleLoadingError(error) {
+  console.warn(error);
+}
+
+function handleFinishLoading(setLoadingComplete) {
+  setLoadingComplete(true);
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
