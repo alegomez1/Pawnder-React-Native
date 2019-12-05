af4ab0c000d86ec913c06ae10fb8263a0558143a
Alejandro Gomez <alejandrogomez@alejandrosimac2.lan>
2019-12-05 12:16:58 -0500

    pet photo upload now updates user in databse

--- src/screens/AddPet.js
+++ src/screens/AddPet.js
@@ -45,12 +45,7 @@ class AddPet extends React.Component {
             quality: 1
         });
 
-        console.log('result----', result);
-
         if (!result.cancelled) {
-            this.setState({ image: result.uri });
-            let newURL = result.uri.replace('file:///', "")
-            console.log('new URL-----', newURL)
             this.uploadDogImage(result)
         }
     }
@@ -67,12 +62,16 @@ class AddPet extends React.Component {
         const res = await fetch(
             'https://api.cloudinary.com/v1_1/pawnder/image/upload',
             {
-              method: 'POST',
-              body: data,
+                method: 'POST',
+                body: data,
             }
         )
         const file = await res.json()
-        console.log('file-----', file)
+            .then((result) => {
+                console.log('photo url-----', result.url)
+                firebase.database().ref(`/users/${this.props.state.currentUser.uid}/pets`)
+                .update({ petPhotoURL: result.url })
+        })
     }
 
     uploadUserImage = async (image) => {
@@ -87,8 +86,8 @@ class AddPet extends React.Component {
         const res = await fetch(
             'https://api.cloudinary.com/v1_1/pawnder/image/upload',
             {
-              method: 'POST',
-              body: data,
+                method: 'POST',
+                body: data,
             }
         )
         const file = await res.json()
@@ -151,17 +150,21 @@ class AddPet extends React.Component {
                             placeholder='City'
                             onChangeText={text => this.setState({ city: text })}
                         />
+
+                        <TouchableOpacity
+                            style={styles.uploadImageButton}
+                            onPress={() => this.pickImage()}>
+                            <Text style={{ fontSize: 14, color: 'white', fontWeight: 'bold' }}>Pick Image</Text>
+                        </TouchableOpacity>
+
+
                         <TouchableOpacity
                             style={styles.nextButton}
                             onPress={() => this.setState({ currentScreen: 'about' })}>
                             <Text style={{ fontSize: 14, color: 'white', fontWeight: 'bold' }}>Next</Text>
                         </TouchableOpacity>
 
-                        <TouchableOpacity
-                            style={styles.nextButton}
-                            onPress={() => this.pickImage()}>
-                            <Text style={{ fontSize: 14, color: 'white', fontWeight: 'bold' }}>Pick Image</Text>
-                        </TouchableOpacity>
+
 
                         <Image source={{ uri: this.state.image }} style={{ width: 200, height: 200 }} />
 
@@ -289,6 +292,16 @@ const styles = StyleSheet.create({
         height: 40,
         textAlign: 'center',
         fontSize: 20
-    }
+    },
+    uploadImageButton:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ed9049',
+        width: 90,
+        height: 30,
+        borderRadius: 10,
+        borderWidth: 1,
+        marginBottom: 15,
+    },
 
 })
