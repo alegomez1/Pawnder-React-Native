61f45230a681bfed6621af676aed69649cdfb086
Alejandro Gomez <alejandrogomez@alejandrosimac2.lan>
2019-12-17 18:45:55 -0500

    Added similar styling to settings page

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -8,26 +8,33 @@ import {
   Text,
   TouchableOpacity,
   View,
+  Dimensions
 } from 'react-native';
 import { connect } from 'react-redux'
 
-import {setUID} from '../actions'
+import { setUID } from '../actions'
 
 import firebase from 'firebase'
 
- class SettingsScreen extends React.Component {
-  render(){
+class SettingsScreen extends React.Component {
+  render() {
 
-  return (
-    <View style={styles.container}>
-      <Text>Settings page</Text>
-      <TouchableOpacity
-      onPress={()=> firebase.auth().signOut()}>
-        <Text>Sign out</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
+    return (
+
+
+      <View style={styles.container}>
+        <View style={styles.headerView}>
+          <Text style={styles.pageHeader}>Settings
+</Text>
+
+        </View>
+        <TouchableOpacity
+          onPress={() => firebase.auth().signOut()}>
+          <Text>Sign out</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 SettingsScreen.navigationOptions = {
@@ -37,7 +44,7 @@ SettingsScreen.navigationOptions = {
 const mapStateToProps = (state) => {
   return { state }
 }
-export default connect(mapStateToProps,{
+export default connect(mapStateToProps, {
   setUID
 })(SettingsScreen)
 
@@ -45,8 +52,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent:'center',
+    alignItems: "center",
+    // justifyContent: 'center'
+  },
+  headerView: {
+    width: Dimensions.get('window').width,
+    // backgroundColor: 'red',
+  },
+  pageHeader: {
+    textAlign: 'center',
+    marginTop: 60,
+    marginBottom: 20,
+    fontSize: 35,
+    fontWeight: 'bold'
   },
 
 });
